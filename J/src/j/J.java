e735a25eae75d020eaf6f0771859c4fe0180a214
acer <acer@acer-PC>
2014-07-18 17:56:16 -0500

    PRIMER COMMIT

---
+++ J/src/j/J.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package j;
+
+/**
+ *
+ * @author acer
+ */
+public class J {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
